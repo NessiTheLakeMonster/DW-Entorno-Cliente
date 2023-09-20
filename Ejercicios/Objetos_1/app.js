5abca36e6033f73a983527f25794fbc1c12dd421
NessiTheLakeMonster <ibarreralle@gmail.com>
2023-09-20 19:42:32 +0200

    Validaciones realizadas

--- Ejercicios/Objetos_1/app.js
+++ Ejercicios/Objetos_1/app.js
@@ -38,23 +38,68 @@ function login() {
 
 }
 
-function comprobarDNI() {
-    var dni = document.getElementById('dni').value;
-    var resto = 0;
-    const l = 'TRWAGMYFPDXBNJZSQVHLCKE';
+// ----------------------------- Funciones comprobaciones de los campos ---------------------------
 
-    //8 numeros y 1 letra
+function comprobarNombre() {
+    var n = document.getElementById('name').value;
 
-    var numeros = dni.substring(0,7);
-    var letra = dni.substring(8);
-
-    resto = numeros % 23;
-
-    if (l[resto] != letra) {
-        alert('DNI esta mal');
+    if (n > 30 || n < 3) {
+        alert('Longitud de nombre no permitido');
     }
+}
 
+function comprobarApellido() {
+    var a1 = document.getElementById('ape1').value;
+    var a2 = document.getElementById('ape2').value;
 
+    if (a1 & a2 > 30 || a1 & a2 < 2) {
+        alert('Longitus de apellidos no permitida');
+    }
+}
+
+function comprobarFecha() {
+    var fechaStr = document.getElementById('nac').value;
+    var fecha = new Date(fechaStr);
+}
+
+function comprobarEmail() {
+    var mail = document.getElementById('mail').value;
+
+    if (mail.includes('@')) {
+        var m = mail.split('@');
+        var usuario = m[0];
+        var dominio = m[1];
+
+        // Compruebo la posicion del @ mediante las longitudes de los campos
+        if (usuario.length == 0 || dominio.length <= 3) {
+            alert('El @ del email no puede estar ni al principio ni en los ultimos tres caracteres');
+        } 
+
+    } else {
+        alert('El email debe incluir un @');
+    }
+}
+
+function comprobarDNI() {
+    // DNI --> 8 numeros y 1 letra
+    var dni = document.getElementById('dni').value.toUpperCase();
+
+    if (dni.length == 9 ) {  // (/^\d{8}[A-Z]$/.test(dni)) {
+        const l = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+        // Divido en substrings
+        var numeros = dni.substring(0,8);
+        var letra = dni.substring(8);
+
+        var resto = numeros % 23;
+
+        if (l[resto] != letra) {
+            alert('El DNI es erroneo');
+        }
+
+    } else {
+        alert('El DNI no tiene un formato válido');
+    }
 }
 
 function limpiar() {
@@ -72,7 +117,11 @@ function guardar() {
 }
 
 function validar() {
-
+    comprobarNombre();
+    comprobarApellido();
+    comprobarEmail();
+    comprobarFecha();
+    comprobarDNI();
 }
 
 // ejemplo le pasa el nombre el min y max y que slaga el error
